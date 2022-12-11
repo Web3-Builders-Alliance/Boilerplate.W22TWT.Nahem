ec402cc13668994bd7fc9b193afbf52631d31edc
nahem <nahemseguias@gmail.com>
2022-12-11 13:54:58 +0100

    uploading template to start off projects quickly with minimal config

--- src/msg.rs
+++ src/msg.rs
@@ -1,103 +1,21 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{StdError, StdResult, Uint128};
 
 #[cw_serde]
-#[cfg_attr(test, derive(Default))]
-pub struct InstantiateMsg {
-    pub commodity: String,
-    pub contract_owner: Option<String>,
-    pub commision: Option<u128>,
-}
-
-impl InstantiateMsg {
-    pub fn validate(&self) -> StdResult<()> {
-        // validate commodity name
-        if !self.has_valid_name() {
-            return Err(StdError::generic_err(
-                "Commodity name is not in the expected format (3-50 UTF-8 bytes)",
-            ));
-        }
-        Ok(())
-    }
-    fn has_valid_name(&self) -> bool {
-        let bytes = self.commodity.as_bytes();
-        if bytes.len() < 3 || bytes.len() > 50 {
-            return false;
-        }
-        true
-    }
-    // TODO: validate contract owner
-    // TODO: validate commission
-}
+pub struct InstantiateMsg {}
 
 #[cw_serde]
 pub enum ExecuteMsg {
-    // any user other than the contract owner can raise their bid by sending tokens to the contract with the Bid {} message
-    Bid {},
-    // only owner can close bid
-    Close {},
-    // sends all senders bids (minus commissions) to the receiver account (if provided) or to the original bidder
-    Retract { receiver: Option<String> },
+    Boilerplate {},
 }
 
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
-    /// Returns the total bid of the given address, 0 if unset.
-    #[returns(TotalBidResponse)]
-    TotalBid { address: String },
-    /// Returns the highest bidder and their total bid amount.
-    #[returns(HighestBidderResponse)]
-    HighestBidder {},
-    /// Returns true if bid is closed.
-    #[returns(IsBidClosedResponse)]
-    IsBidClosed {},
-    /// Returns the highest bidder and their total bid amount.
-    #[returns(BidWinnerResponse)]
-    BidWinner {},
+    /// Returns BoilerplateResponse
+    #[returns(BoilerplateResponse)]
+    Boilerplate {},
 }
 
 // We define a custom struct for each query response
 #[cw_serde]
-pub struct TotalBidResponse {
-    pub total_bid: Uint128,
-}
-
-#[cw_serde]
-pub struct HighestBidderResponse {
-    pub addr: String,
-    pub total_bid: Uint128,
-}
-
-#[cw_serde]
-pub struct IsBidClosedResponse {
-    pub is_closed: bool,
-}
-
-#[cw_serde]
-pub struct BidWinnerResponse {
-    pub winner: String,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn validate_instantiate_msg_name() {
-        // Too short
-        let mut msg = InstantiateMsg {
-            commodity: str::repeat("a", 2),
-            ..InstantiateMsg::default()
-        };
-        assert!(!msg.has_valid_name());
-
-        // In the correct length range
-        msg.commodity = str::repeat("a", 3);
-        assert!(msg.has_valid_name());
-
-        // Too long
-        msg.commodity = str::repeat("a", 51);
-        assert!(!msg.has_valid_name());
-    }
-}
+pub struct BoilerplateResponse {}
--- src/query.rs
+++ src/query.rs
@@ -1,46 +1,9 @@
-use cosmwasm_std::{Deps, StdError, StdResult};
+use cosmwasm_std::{Deps, StdResult};
 
-use crate::{
-    msg::{BidWinnerResponse, HighestBidderResponse, IsBidClosedResponse, TotalBidResponse},
-    state::{BidStatus, BIDS, CONFIG, WINNER},
-};
+use crate::{msg::BoilerplateResponse, state::CONFIG};
 
-pub fn highest_bid(deps: Deps) -> StdResult<HighestBidderResponse> {
-    // if maps is small, might be cleaner to iterate through the bids and collect the highest.
-    let winner = WINNER.load(deps.storage)?;
+pub fn boilerplate(deps: Deps) -> StdResult<BoilerplateResponse> {
+    let _cfg = CONFIG.load(deps.storage)?;
 
-    Ok(HighestBidderResponse {
-        addr: winner.0.to_string(),
-        total_bid: winner.1.to_owned(),
-    })
-}
-
-pub fn total_bid(deps: Deps, addr: String) -> StdResult<TotalBidResponse> {
-    // if maps is small, might be cleaner to iterate through the bids and collect the highest.
-    let bid = BIDS.load(deps.storage, deps.api.addr_validate(&addr)?)?;
-
-    Ok(TotalBidResponse { total_bid: bid })
-}
-
-pub fn is_closed(deps: Deps) -> StdResult<IsBidClosedResponse> {
-    let cfg = CONFIG.load(deps.storage)?;
-
-    match cfg.status {
-        BidStatus::Closed => Ok(IsBidClosedResponse { is_closed: true }),
-        BidStatus::Opened => Ok(IsBidClosedResponse { is_closed: false }),
-    }
-}
-
-pub fn bid_winner(deps: Deps) -> StdResult<BidWinnerResponse> {
-    let cfg = CONFIG.load(deps.storage)?;
-
-    // return error if bid is not closed
-    if cfg.status != BidStatus::Closed {
-        return Err(StdError::generic_err("Bid is not closed"));
-    }
-
-    let winner = WINNER.load(deps.storage)?;
-    Ok(BidWinnerResponse {
-        winner: winner.0.to_string(),
-    })
+    Ok(BoilerplateResponse {})
 }
